a954363ea9d5ee8516e57ecaed88cf5846863112
phillfei <139048738+phillfei@users.noreply.github.com>
2025-01-31 11:53:53 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     net.to(device=device)
     state_dict = torch.load(args.model, map_location=device)
     # mask_values = state_dict.pop('mask_values', [0, 1])
-    net.load_state_dict(state_dict)
+    net.load_state_dict(state_dict,strict=False)
     print(sum(p.numel() for p in net.parameters()))
     logging.info('Model loaded!')
     if args.datasets == 'ISIC':
